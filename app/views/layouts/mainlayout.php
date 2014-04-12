1d4ea52aa8a73e55def84db8c7b6cfade1917056
Gabriel Mendonça <gabrielmendonca@gmail.com>
2014-04-12 03:53:49 -0300

    adicionando bootstrap3 aos arquivos do framework

--- app/views/layouts/mainlayout.php
+++ app/views/layouts/mainlayout.php
@@ -1,30 +1,32 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt">
 	<head>
 		<title>
 			<?php echo $pagename; ?>
 		</title>
+
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap-theme.min.css" />
 	</head>
 	<body>
 
-		<h1> <?php echo $title; ?></h1>
+		<?php 
+			if(is_array($body)){
+				foreach ($body as $b => $value) {
+					echo $value;
+				}
+			}
+			else{
+				echo $body;
+			}
+		 ?>
 
-		<div class="container">
-			<div class="content">
-				
-				<?php 
-					if(is_array($body)){
-						foreach ($body as $b => $value) {
-							echo $value;
-						}
-					}
-					else{
-						echo $body;
-					}
-				 ?>
-
-			</div>
-		</div>
+		 <script type="text/javascript" src="assets/jquery/jquery.min.js"></script>
+		 <script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
 
 	</body>
 </html>
